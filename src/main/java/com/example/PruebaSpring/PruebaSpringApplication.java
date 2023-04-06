934f4c7ecce09710501af66199b236573cc4199a
Alex Carrera <a_fcalvarez@hotmail.com>
2023-04-06 16:23:17 -0500

    hello world

--- src/main/java/com/example/PruebaSpring/PruebaSpringApplication.java
+++ src/main/java/com/example/PruebaSpring/PruebaSpringApplication.java
@@ -2,10 +2,18 @@ package com.example.PruebaSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class PruebaSpringApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaSpringApplication.class, args);
 	}
